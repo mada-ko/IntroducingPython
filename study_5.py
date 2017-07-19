1f996de48d1e3341b9e006c88f9b79d5da839cee
aa369876 <aa369876@aa369876noAir.dhcp.osaka.japan.ibm.com>
2017-07-19 14:39:42 +0900

    modify study_5

--- study_5.py
+++ study_5.py
@@ -1,1 +1,29 @@
 # study5-1
+import zoo
+zoo.hours()
+
+# 5-2
+import zoo as menagerie
+menagerie.hours()
+
+# 5-3
+from zoo import hours
+hours()
+
+# 5-4
+from zoo import hours as info
+
+# 5-5
+plain = {'a': 1, 'b': 2, 'c': 3}
+print(plain)
+
+# 5-6
+from collections import OrderedDict
+fancy = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+print(fancy)
+
+# 5-7
+from collections import defaultdict
+dict_of_lists = defaultdict(list)
+dict_of_lists['a'].append('something for a')
+print(dict_of_lists['a'])
